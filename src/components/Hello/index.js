07295a5763f7410025c7a7e30dcb196422f16827
Zachary Larson <Zach@Zacharys-MBP.attlocal.net>
2021-08-09 15:49:03 -0400

    image cropper now functional

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import icon from '../../../assets/icon.svg'
+
 
 const Hello = () => {
     console.info('We are using Electron!')
     return (
       <div>
-        <div className="Hello">
-          <img width="200px" alt="icon" src={icon} />
-        </div>
-        <h1>My first desktop app</h1>
+        <h1>Image Cropper</h1>
         <div className="Hello">
           <Link to="/photos">
             <button type="button">
--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,20 +1,59 @@
-import React, {useState} from 'react'
+import React, {useState, useCallback} from 'react'
 import Cropper from 'react-easy-crop'
+import { readFile, saveCroppedImage } from '../../helpers/images'
 
 
 function Photos() {
+    const [imageSrc, setImageSrc] = useState(null)
     const [crop, setCrop] = useState({ x:0, y:0})
     const [zoom, setZoom] = useState(1)
+    const [fileName, setFileName] = useState(null)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+
+    const onCropComplete = useCallback((croppedArea, _croppedAreaPixels) => {
+        setCroppedAreaPixels(_croppedAreaPixels)
+    }, [])
+
+    const handleFileChange = async (e) => {
+        if(e.target.files && e.target.files.length>0){
+            const file = e.target.files[0]
+            setFileName(file.path)
+            const imageDataUrl = await readFile(file)
+            setImageSrc(imageDataUrl)
+        }
+    }
+
+    const handleSave = () => {
+        // first - we want to save the cropped image
+        // new function that takes croppedAreaPixelsand fileName and imageSrc
+        saveCroppedImage(fileName, imageSrc, croppedAreaPixels)
+        setImageSrc(null)
+        setZoom(1)
+        setCrop({x: 0, y: 0})
+    }
+
+    if(!imageSrc) {
+        return (
+        <>
+        <h1>Please choose a photo to crop</h1>
+        <input type="file" accept="image/*" onChange={handleFileChange} />
+        </>
+        )
+    }
 
     return (
+        <div className="crop-area">
         <Cropper
         crop={crop}
         zoom={zoom}
         aspect={4 / 3}
         onCropChange={setCrop}
         onZoomChange={setZoom}
-        image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+        onCropComplete={onCropComplete}
+        image={imageSrc}
         />
+        <button className="save-btn" onClick={handleSave}>Save</button>
+        </div>
     )
 }
 
